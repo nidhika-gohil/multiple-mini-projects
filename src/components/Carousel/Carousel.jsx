7f0a5baf50deb1b0e2dd8155b4a8288db63789ec
Nidhika Gohil <KTPL-0132@kpoint.co.in>
2024-06-24 20:49:20 +0530

    Adding logic of bullet and its click

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -18,6 +18,10 @@ const Carousel = () => {
     });
   };
 
+  const handleBulletClick = (index) => {
+    setCurrentIndex(index);
+  };
+
   useEffect(()=> {
     const timer = setInterval(() => {
       handleNextClick();
@@ -32,20 +36,30 @@ const Carousel = () => {
   return (
     photosData === undefined || photosData.length === 0  ? (<div>No Data found</div>) : 
     (
-      <div className="main-carousel-container">
-        <button onClick={handlePrevClick}> Prev </button>
-        {
-          photosData.map((photo, index) => {
-            return (
-              <div className={currentIndex === index ? "block" : "d-none"} key={photo.id}>
-                {/* {console.log("Indexxxxxxxxxxxxxxx => ",currentIndex)} */}
-                <img src={photo.urls.regular} alt= {photo.alt_description} width={300} height={400} />
-              </div>
-            )
-          })
-        }
-        <button onClick={handleNextClick}> Next </button>
-      </div>
+      <>
+        <div className="main-carousel-container">
+          <button onClick={handlePrevClick}> Prev </button>
+          {
+            photosData.map((photo, index) => {
+              return (
+                <div className={currentIndex === index ? "block" : "d-none"} key={photo.id}>
+                  {/* {console.log("Indexxxxxxxxxxxxxxx => ",currentIndex)} */}
+                  <img src={photo.urls.regular} alt= {photo.alt_description} width={300} height={400} />
+                </div>
+              )
+            })
+          }
+          <button onClick={handleNextClick}> Next </button>
+        </div>
+        <div className="bullet-container">
+          {
+            photosData.map((photo, index) => {
+              return (<div className={"bullet " + (currentIndex === index ? "active" : "")} onClick={() => handleBulletClick(index)}>
+              </div>)
+            })
+          }
+        </div>
+      </>
     )
   )
 };
